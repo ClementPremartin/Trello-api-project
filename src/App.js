5e98fcee7d1c6a35bfe925d856b60842ae506e49
ClementPremartin <clementpremartin@gmail.com>
2022-04-28 11:31:00 +0200

    adding style in app.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import axios from 'axios'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from "./components/Card.jsx";
 
 function App() {
-  const [cards, setCards] = useState();
+  const [cards, setCards] = useState([]);
   
   const generateCards = (listId) => {
-    axios.get(`https://api.trello.com/1/lists/${listId}/cards?key=5657b57008134bb95cf12e85caa61c71&token=7bac74fb60f2277495928b6ebe21f7e297731b1a4aa870e3a2fed171b59a5e58`)
+    axios.get(`https://api.trello.com/1/lists/${listId}/cards?key=${process.env.REACT_APP_TRELLO_KEY}&token=${process.env.REACT_APP_TRELLO_TOKEN}`)
     .then((response) => 
     setCards(response.data))
   }
@@ -21,13 +21,15 @@ function App() {
             <h1>Our favourite Trello cards</h1>
           </div>
         </div>
+        <div className='buttons-container'>
         <button className="Comptines" onClick={() => generateCards("6267adf924d93260bd52264b")}>Comptines enfants</button>
         <button className="Metal" onClick={() => generateCards("6267b5e9f27af9730c2f496a")}>Comptines adultes</button>
+        </div>
         <div className='row-cards'>
           {cards ? cards.map(e => <Card elements={e} />) : console.log("Oupsi")}
         </div>
       </div>
-    </main>
+    </main> 
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,8 @@ function Card({elements}) {
   const [picture, setPicture] = useState("");
   useEffect(() => {
     console.log(elements);
-    if (elements.cover) {axios.get(`https://api.trello.com/1/cards/${elements.id}/attachments/${elements.cover?.idAttachment}?key=5657b57008134bb95cf12e85caa61c71&token=7bac74fb60f2277495928b6ebe21f7e297731b1a4aa870e3a2fed171b59a5e58`).then((response) => setPicture(response.data.previews[4].url));
+    if (elements.cover) {axios.get(`https://api.trello.com/1/cards/${elements.id}/attachments/${elements.cover?.idAttachment}?key=${process.env.REACT_APP_TRELLO_KEY}&token=${process.env.REACT_APP_TRELLO_TOKEN}`)
+    .then((response) => setPicture(response.data.previews[4].url));
   }
   },[elements]);
 //
@@ -15,7 +16,7 @@ function Card({elements}) {
       <div className='card card-white'>
         <div className='card-content'>
             <img src={picture} className='card-img-top' alt="trello feature" />
-          <h1>{elements.name}</h1>
+          <h2>{elements.name}</h2>
           <p>{elements.desc}</p>
         </div>
       </div>  
